8bd5c6c965a3aa21c4ee9310f9778171a73219c7
Mattia Paterna <mpa@dixa.com>
2022-03-04 16:12:03 +0100

    feat: refactor main script
    The script can be seen as entrypoint for local testing
    of the pipeline. The `main` function takes a config
    dict used to set different variables in the pipeline e.g.
    dataset structure, I/O, model specs training params etc.
    
    Moreover, l. 15 does the experiment tracking inside
    Mlflow platform.

--- script.py
+++ script.py
@@ -1,159 +1,47 @@
-import tensorflow as tf
-import os
-import wave
-import pylab
-from pathlib import Path
+import argparse
 
-# Set paths to input and output data
-INPUT_DIR = "recordings/"
-OUTPUT_DIR = "working/"
-os.makedirs(OUTPUT_DIR, exist_ok=True)
+import mlflow
 
-# Print names of 10 WAV files from the input path
-parent_list = os.listdir(INPUT_DIR)
-for i in range(10):
-    print(parent_list[i])
-
-# ================================================== #
-#       Convert audio files to spectograms           #
-# ================================================== #
-
-# Utility function to get sound and frame rate info
-def get_wav_info(wav_file):
-    wav = wave.open(wav_file, "r")
-    frames = wav.readframes(-1)
-    sound_info = pylab.frombuffer(frames, "int16")
-    frame_rate = wav.getframerate()
-    wav.close()
-    return sound_info, frame_rate
+from src.utils.io_utils import load_config
+from steps import dataset_dir_structure, process_audio, create_mnist_audio_dataset, build_mnist_audio_model, \
+    split_dataset, train_and_evaluate
 
 
-# For every recording, make a spectogram and save it as label_speaker_no.png
-if not os.path.exists(os.path.join(OUTPUT_DIR, "audio-images")):
-    os.mkdir(os.path.join(OUTPUT_DIR, "audio-images"))
-
-for filename in os.listdir(INPUT_DIR):
-    if "wav" in filename:
-        file_path = os.path.join(INPUT_DIR, filename)
-        file_stem = Path(file_path).stem
-        target_dir = f"class_{file_stem[0]}"
-        dist_dir = os.path.join(os.path.join(OUTPUT_DIR, "audio-images"), target_dir)
-        file_dist_path = os.path.join(dist_dir, file_stem)
-        if not os.path.exists(file_dist_path + ".png"):
-            if not os.path.exists(dist_dir):
-                os.mkdir(dist_dir)
-            file_stem = Path(file_path).stem
-            sound_info, frame_rate = get_wav_info(file_path)
-            pylab.specgram(sound_info, Fs=frame_rate)
-            pylab.savefig(f"{file_dist_path}.png")
-            pylab.close()
-
-# Print the ten classes in our dataset
-path_list = os.listdir(os.path.join(OUTPUT_DIR, "audio-images"))
-print("Classes: \n")
-for i in range(10):
-    print(path_list[i])
-
-# File names for class 1
-path_list = os.listdir(os.path.join(OUTPUT_DIR, "audio-images/class_1"))
-print("\nA few example files: \n")
-for i in range(5):
-    print(path_list[i])
-
-# ================================================== #
-#                 Preparing data                     #
-# ================================================== #
-
-# Declare constants
-IMAGE_HEIGHT = 256
-IMAGE_WIDTH = 256
-BATCH_SIZE = 32
-N_CHANNELS = 3
-N_CLASSES = 10
-
-# Make a dataset containing the training spectrograms
-train_dataset = tf.keras.preprocessing.image_dataset_from_directory(
-    batch_size=BATCH_SIZE,
-    validation_split=0.2,
-    directory=os.path.join(OUTPUT_DIR, "audio-images"),
-    shuffle=True,
-    color_mode="rgb",
-    image_size=(IMAGE_HEIGHT, IMAGE_WIDTH),
-    subset="training",
-    seed=0,
-)
-
-# Make a dataset containing the validation spectrogram
-valid_dataset = tf.keras.preprocessing.image_dataset_from_directory(
-    batch_size=BATCH_SIZE,
-    validation_split=0.2,
-    subset="validation",
-    seed=42,
-    directory=os.path.join(OUTPUT_DIR, "audio-images"),
-    shuffle=True,
-    color_mode="rgb",
-)
+# Main flow
+def main(cfgs: dict):
+    dataset_path = dataset_dir_structure(cfgs.get("dataset_structure"))
+    # Process audio files into mel-spectrograms
+    process_audio(cfgs.get("processor"), dataset_path)
+    with mlflow.start_run():
+        # Build MNIST audio dataset
+        tf_dataset = create_mnist_audio_dataset(dataset_path)
+        # Build train and validation datasets
+        validation_split = cfgs.get("model").get("training").get("validation_split")
+        training_dataset, validation_dataset = split_dataset(
+            tf_dataset.shuffle(buffer_size=50, seed=42),
+            validation_split
+        )
+        # Create and compile model
+        mnist_model = build_mnist_audio_model(cfgs.get("model"))
+        # Train and evaluate
+        _, evaluation = train_and_evaluate(
+            model=mnist_model,
+            train_dataset=training_dataset,
+            validation_dataset=validation_dataset,
+            train_cfg=cfgs.get("model").get("training")
+        )
 
 
-def prepare(ds, augment=False):
-    # Define our one transformation
-    rescale = tf.keras.Sequential([tf.keras.layers.experimental.preprocessing.Rescaling(1.0 / 255)])
-    flip_and_rotate = tf.keras.Sequential(
-        [
-            tf.keras.layers.experimental.preprocessing.RandomFlip("horizontal_and_vertical"),
-            tf.keras.layers.experimental.preprocessing.RandomRotation(0.2),
-        ]
-    )
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Run the audio preprocessing pipeline for the input directory.")
+    parser.add_argument("--config", type=str, required=True, help="YAML config file")
 
-    # Apply rescale to both datasets and augmentation only to training
-    ds = ds.map(lambda x, y: (rescale(x, training=True), y))
-    if augment:
-        ds = ds.map(lambda x, y: (flip_and_rotate(x, training=True), y))
-    return ds
+    args = parser.parse_args()
+    config = load_config(args.config)
 
+    mlflow.tensorflow.autolog()
 
-train_dataset = prepare(train_dataset, augment=False)
-valid_dataset = prepare(valid_dataset, augment=False)
+    mlflow.create_experiment(config.get("experiment_name"))
+    mlflow.set_experiment(config.get("experiment_name"))
 
-# ================================================== #
-#                 Modelling                          #
-# ================================================== #
-
-# Create CNN model
-model = tf.keras.models.Sequential()
-model.add(tf.keras.layers.Input(shape=(IMAGE_HEIGHT, IMAGE_WIDTH, N_CHANNELS)))
-model.add(tf.keras.layers.Conv2D(32, 3, strides=2, padding="same", activation="relu"))
-model.add(tf.keras.layers.BatchNormalization())
-model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
-model.add(tf.keras.layers.BatchNormalization())
-model.add(tf.keras.layers.Conv2D(64, 3, padding="same", activation="relu"))
-model.add(tf.keras.layers.BatchNormalization())
-model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
-model.add(tf.keras.layers.BatchNormalization())
-model.add(tf.keras.layers.Conv2D(128, 3, padding="same", activation="relu"))
-model.add(tf.keras.layers.BatchNormalization())
-model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
-model.add(tf.keras.layers.BatchNormalization())
-model.add(tf.keras.layers.Flatten())
-model.add(tf.keras.layers.Dense(256, activation="relu"))
-model.add(tf.keras.layers.BatchNormalization())
-model.add(tf.keras.layers.Dropout(0.5))
-model.add(tf.keras.layers.Dense(N_CLASSES, activation="softmax"))
-
-# Compile model
-model.compile(
-    loss="sparse_categorical_crossentropy",
-    optimizer=tf.keras.optimizers.RMSprop(),
-    metrics=["accuracy"],
-)
-
-# Train model for 10 epochs, capture the history
-history = model.fit(train_dataset, epochs=1, validation_data=valid_dataset)
-
-# ================================================== #
-#                 Evaluation                         #
-# ================================================== #
-
-# Compute the final loss and accuracy
-final_loss, final_acc = model.evaluate(valid_dataset, verbose=0)
-print("Final loss: {0:.6f}, final accuracy: {1:.6f}".format(final_loss, final_acc))
+    main(config)
